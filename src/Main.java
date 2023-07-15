021effbcd7801024cacce1cc663923e3dfb14d77
Sefali21 <100235906+Sefali21@users.noreply.github.com>
2023-07-15 15:58:14 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Eingabe with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int numberMax;
+        Scanner scanner=new Scanner(System.in);
+        numberMax= scanner.nextInt();
+        int limit=3;
+        ArrayList<Integer> primeList=new ArrayList<>();
+        primeList.add(2);
+        while (limit!=numberMax+1) {
+            boolean flag=true;
+            for (int i = 2; i <limit ; i++) {
+                if(limit%i==0){
+                    flag=false;
+                    break;}
+            }
+            System.out.println(flag+" "+limit);
+            if (flag){
+                primeList.add(limit);
+            }
+            limit++;
 
-        // Press Umschalt+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Umschalt+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Strg+F8.
-            System.out.println("i = " + i);
+        }
+        for (var c:primeList ) {
+            System.out.println(c+" ");
         }
     }
 }
